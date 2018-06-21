556bac5454ba7ce0a4f027a7b3d4d565dc2088ea
Martin Medovarsky <med0001@r3-04.viewbank-co.wan>
2018-06-21 12:42:21 +1000

    finally fixed font

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 1400, height: 1000})
   
     // and load the index.html of the app.
     win.loadFile('index.html')
