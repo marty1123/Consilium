7c5277be3a2de0eaf6335bfc28bf253ba55135ab
marty1123 <40352579+marty1123@users.noreply.github.com>
2018-07-22 19:34:44 +1000

    Introduced modal and select

--- dist/main.js
+++ dist/main.js
@@ -2,50 +2,30 @@ var $ = require("jquery");
 const {app, BrowserWindow} = require('electron')
 //window.$ = window.jQuery = require('jquery');
   
-  // Keep a global reference of the window object, if you don't, the window will
-  // be closed automatically when the JavaScript object is garbage collected.
   let win
   
   function createWindow () {
-    // Create the browser window.
     //var viewport = require('electron-viewport')(950, 1025, {resizable:false})
     //var win = viewport.getWindow()
 
-    win = new BrowserWindow({width: 950, height: 1025, minWidth: 950})
+    win = new BrowserWindow({width: 970, height: 1025, minWidth: 970})
     
-  
-    // and load the index.html of the app.
     win.loadFile('index.html')
-
-    //Removes menu bar from window.
-    //win.setMenu(null)
   
-    // Emitted when the window is closed.
     win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
       win = null
     })
   }
   
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
   app.on('ready', createWindow)
   
-  // Quit when all windows are closed.
   app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
       app.quit()
     }
   })
   
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow()
     }
